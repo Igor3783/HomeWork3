f7a44d163897d6fdd5fee627ad8d50373efe4ff0
Igor <monaxnew@rambler.ru>
2023-02-05 15:21:36 +0300

    aad work3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int number =Convert.ToInt32(Console.ReadLine());
+int num = number % 2;
+if (num == 0){
+    Console.WriteLine("Числло делится без остатка");
+}
+else{
+    Console.WriteLine("Не делится без остатка");
+}
